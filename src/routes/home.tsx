915000295a5831738f61f1eb3012e9de7f94db00
Fred <frederik.koenig@posteo.de>
2025-05-15 17:27:11 +0200

    adjusted contacts

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -55,26 +55,28 @@ export function Home() {
             variants={itemVariants}
           />
         </motion.div>
-        <motion.div className="mail flex flex-col variants={itemVariants} px-5 md:px-[38vw]">
-          <motion.div variants={itemVariants} className="flex flex-row gap-4">
-            <FaPhoneAlt />
-            <motion.a
-              href="tel:+491623231807"
-              className="hover:text-fsorange hover:underline"
-            >
-              0162 3231807
-            </motion.a>
+        <div className="flex justify-center">
+          <motion.div className="mail flex flex-col variants={itemVariants}">
+            <motion.div variants={itemVariants} className="flex flex-row gap-4">
+              <FaPhoneAlt />
+              <motion.a
+                href="tel:+491623231807"
+                className="hover:text-fsorange hover:underline"
+              >
+                0162 3231807
+              </motion.a>
+            </motion.div>
+            <motion.div variants={itemVariants} className="flex flex-row gap-3">
+              <FaEnvelope className="mr-2" />
+              <motion.a
+                href="mailto:toege@feldhoff-solar.de"
+                className="hover:text-fsorange hover:underline"
+              >
+                toege@feldhoff-solar.de
+              </motion.a>
+            </motion.div>
           </motion.div>
-          <motion.div variants={itemVariants} className="flex flex-row gap-3">
-            <FaEnvelope className="mr-2" />
-            <motion.a
-              href="mailto:toege@feldhoff-solar.de"
-              className="hover:text-fsorange hover:underline"
-            >
-              toege@feldhoff-solar.de
-            </motion.a>
-          </motion.div>
-        </motion.div>
+        </div>
       </motion.div>
     </div>
   );
